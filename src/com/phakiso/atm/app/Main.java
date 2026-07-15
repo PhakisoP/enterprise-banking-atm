5375f61fe225b8bba797badd005735f3899a7fab
Phakiso Pitso <phakiso.pitso@gmail.com>
2026-07-15 15:10:09 +0200

    Initial project structure and application entry point

---
+++ src/com/phakiso/atm/app/Main.java
@@ -0,0 +1,14 @@
+package com.phakiso.atm.app;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("======================================");
+        System.out.println("     ENTERPRISE BANKING ATM SYSTEM");
+        System.out.println("======================================");
+        System.out.println("Welcome to Enterprise Banking.");
+        System.out.println();
+        System.out.println("System Initialised Successfully.");
+    }
+}
