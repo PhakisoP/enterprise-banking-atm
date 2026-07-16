2e051fd636cdd8cf26ce90fb281daf28a9fa396a
Phakiso Pitso <phakiso.pitso@gmail.com>
2026-07-16 18:21:59 +0200

    Add Customer constructor

--- src/com/phakiso/atm/app/Main.java
+++ src/com/phakiso/atm/app/Main.java
@@ -1,14 +1,21 @@
 package com.phakiso.atm.app;
 
+import com.phakiso.atm.model.Customer;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("======================================");
-        System.out.println("     ENTERPRISE BANKING ATM SYSTEM");
-        System.out.println("======================================");
-        System.out.println("Welcome to Enterprise Banking.");
-        System.out.println();
-        System.out.println("System Initialised Successfully.");
+        Customer customer = new Customer(
+                1,
+                "Phakiso",
+                "Pitso",
+                "9001015009088",
+                "0812345678",
+                "phakiso@email.com"
+        );
+
+        System.out.println("Customer created successfully!");
     }
+
 }
--- src/com/phakiso/atm/model/Customer.java
+++ src/com/phakiso/atm/model/Customer.java
@@ -9,4 +9,18 @@ public class Customer {
     private String phoneNumber;
     private String email;
 
+    public Customer(int customerId,
+                    String firstName,
+                    String lastName,
+                    String idNumber,
+                    String phoneNumber,
+                    String email) {
+
+        this.customerId = customerId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.idNumber = idNumber;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+    }
 }
