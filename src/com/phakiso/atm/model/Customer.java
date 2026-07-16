e5a74c9115888a2fa41dbaf071c5202e9474e59e
Phakiso Pitso <phakiso.pitso@gmail.com>
2026-07-16 17:32:45 +0200

    Create Customer model class

---
+++ src/com/phakiso/atm/model/Customer.java
@@ -0,0 +1,12 @@
+package com.phakiso.atm.model;
+
+public class Customer {
+
+    private int customerId;
+    private String firstName;
+    private String lastName;
+    private String idNumber;
+    private String phoneNumber;
+    private String email;
+
+}
